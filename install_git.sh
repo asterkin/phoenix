a0b405ebfb0debc279606d6c12d2e60aa493c894
asterkin <asher.sterkin@gmail.com>
2014-01-02 16:39:45 +0200

    Update install_git.sh
    quotes removed

--- install_git.sh
+++ install_git.sh
@@ -1,5 +1,5 @@
 apt-get -y install git
 apt-get -y install gitk
-git config --global --add user.name "$1"
-git config --global --add user.mail "$2"
+git config --global --add user.name $1
+git config --global --add user.mail $2
 
